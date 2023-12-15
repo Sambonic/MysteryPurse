929ab79879469a65a449431a04a99c3d62f2a663
Sambonic <124459402+Sambonic@users.noreply.github.com>
2023-12-15 20:58:20 +0200

    Updated Canvas Sizes

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
+    public Transform Target;
+    public float Cameraspeed;
+
+    public float minX, maxX;
+    public float minY, maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
     void FixedUpdate()
     {
-
-
-        Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
-        transform.position = Vector3.Lerp(transform.position, targetPos, 0.2f);
-
+        if (Target != null)
+        {
+            Vector2 newCamPosition = Vector2.Lerp(transform.position, Target.position, Time.deltaTime * Cameraspeed);
+            float ClampX = Mathf.Clamp(newCamPosition.x, minX, maxX);
+            float ClampY = Mathf.Clamp(newCamPosition.y, minY, maxY);
+            transform.position = new Vector3(ClampX, ClampY, -10f);
+        }
 
     }
 }
